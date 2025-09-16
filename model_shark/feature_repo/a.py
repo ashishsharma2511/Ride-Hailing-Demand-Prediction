9e93a0bff442f17366f386f1a9a13c171e06b905
ashishsharma2511 <73204628+ashishsharma2511@users.noreply.github.com>
2025-09-15 22:25:53 -0400

    ds

--- model_shark/feature_repo/a.py
+++ model_shark/feature_repo/a.py
@@ -1,17 +1,3 @@
-import pandas as pd
-import os
-DATA_PATH = os.path.join(os.path.dirname(__file__), "data/ride_requests.csv")
-# Read CSV
-df = pd.read_csv(DATA_PATH)
-
-df['event_timestamp'] = pd.to_datetime(df['event_timestamp'], utc=True)
-
-# Optional: if you have created_timestamp column, convert it as well
-# df['created_timestamp'] = pd.to_datetime(df['created_timestamp'], utc=True)
-
-# Verify the conversion
-print(df.dtypes)
-print(df.head())
-DATA_PATH = os.path.join(os.path.dirname(__file__), "data/ride_requests.parquet")
-# Save back to CSV or Parquet (Parquet recommended for Feast)
-df.to_parquet(DATA_PATH, index=False)
+from feast import FeatureStore
+store = FeatureStore(repo_path="model_shark/feature_repo")
+print(store.list_feature_views())
